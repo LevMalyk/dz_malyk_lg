9108ca5b12cff16d8d9e08f71f7a61a4eaedaaca
Lev <tpr.transenergo@gmail.com>
2024-08-30 19:11:59 +1000

    add new function in widget.py

---
+++ src/widget.py
@@ -0,0 +1,18 @@
+from typing import Union
+
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(user_data: Union[str, int]) -> str:
+    """
+    Функция получает данные от пользователя и маскирует их.
+    :param user_data: Данные полученные от пользователя
+    :return: Результат маскировки данных или сообщение об ошибке
+    """
+    user_data = str(user_data)
+    if user_data[-20:].isdigit():
+        return f"{user_data[:-20]}{get_mask_account(user_data[-20:])}"
+    elif user_data[-16:].isdigit():
+        return f"{user_data[:-16]}{get_mask_card_number(user_data[-16:])}"
+    else:
+        return "Введены неправельные данные"
