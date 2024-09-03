e80045ad2980c4b63ec49d9b440397adc34779b3
Lev <tpr.transenergo@gmail.com>
2024-09-04 01:33:58 +1000

    add new function in src/processing.py

---
+++ src/processing.py
@@ -0,0 +1,6 @@
+def filter_by_state(dict_banking_operations: dict, state: str = 'EXECUTED') -> dict:
+    pass
+
+
+def sort_by_date(dict_banking_operations: dict, sort_param: bool = True) -> dict:
+    pass
